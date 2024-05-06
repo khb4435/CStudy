a4f9cb07a852df7bc764bdf5faa684f2ec391b28
hbkim <khb4435@naver.com>
2024-05-07 00:11:56 +0900

    df

--- C언어100제/4.마름모출력하기/main.cpp
+++ C언어100제/4.마름모출력하기/main.cpp
@@ -1,21 +1,26 @@
 /*
-	*
-   ***
-  *****
-   ***
-    * 
+ 
+##*
+#***
+*****
+#***
+##*
+
 */
 
 #include <stdio.h>
-
-//void main()
-//{
-//	for (int i = 5; i >= 0; i--)
-//	{
-//		for ()
-//		{
-//
-//		}
-//		printf("\n");
-//	}
-//}
+void main()
+{
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 1-i; j++)
+		{
+			printf("#");
+		}
+		for (int k = 0; k < 2 * i + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
